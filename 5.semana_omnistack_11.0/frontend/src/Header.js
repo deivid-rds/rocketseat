5ce3a67dda84551f16f5c0515fce8cd747fcd4ee
Deivid Souza <ddeividd.eu@hotmail.com>
2020-03-27 18:09:27 -0300

    Usando Header especificando a propriedade

--- 5.semana_omnistack_11.0/frontend/src/Header.js
+++ 5.semana_omnistack_11.0/frontend/src/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Header(props){
+export default function Header({ children }){
     return (
         <header>
            <h1>{props.children}</h1> 
