8ec0deaab5017da868a9e832ad8a41f1ca5d092d
Deivid Souza <ddeividd.eu@hotmail.com>
2020-03-27 12:04:16 -0300

    Reorganizar rotas

--- 5.semana_omnistack_11.0/backend/src/routes.js
+++ 5.semana_omnistack_11.0/backend/src/routes.js
@@ -1,34 +1,13 @@
 const express = require('express');
-const crypto = require('crypto');
 
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
 
 const routes = express.Router();
 
 // Listar ongs
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*');
-
-    return response.json(ongs);
-});
+routes.get('/ongs', OngController.index);
 
 // Cadastrar ongs
-routes.post('/ongs', async (request, response) => {
-
-    const { name, email, whatsapp, city, uf } = request.body;
-
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    })
-
-    return response.json({ id });
-});
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
