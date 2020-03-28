69a2e6bdd3b89f55ae25495fcecb4bed14045898
Deivid Souza <ddeividd.eu@hotmail.com>
2020-03-27 22:46:22 -0300

    Exemplo de contador para explicar Estado em React

--- 5.semana_omnistack_11.0/frontend/src/App.js
+++ 5.semana_omnistack_11.0/frontend/src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 
 // JSX (Java Script XML) = Quando temos HTML numa página .js
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  // useState retorna array com 2 posições: Array [valor, funcaoDeAtualização]
+
+  function increment(){
+    setCounter(counter + 1);
+  }
+
   return (
-    <Header>
-      Semana OmniStack
-    </Header>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
