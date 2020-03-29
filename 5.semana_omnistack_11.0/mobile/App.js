9f34a715ad980581d6e897b86d5a71b2965c8ac9
Deivid Souza <ddeividd.eu@hotmail.com>
2020-03-29 11:29:56 -0300

    Configuração inicial e separação em pastas

--- 5.semana_omnistack_11.0/mobile/App.js
+++ 5.semana_omnistack_11.0/mobile/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello Omnistack</Text>
+    <View>
+      <Text>Hello Omnistack</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text:{
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold'
-  }
-});
