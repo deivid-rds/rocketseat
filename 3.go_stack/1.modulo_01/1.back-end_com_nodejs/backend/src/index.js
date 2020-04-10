692ed31b571ca8c71cadc47fc918742a3154bbe0
Deivid Souza <ddeividd.eu@hotmail.com>
2020-04-10 20:50:42 -0300

    Hello World sem rota e com json

--- 3.go_stack/1.modulo_01/1.back-end_com_nodejs/backend/src/index.js
+++ 3.go_stack/1.modulo_01/1.back-end_com_nodejs/backend/src/index.js
@@ -3,8 +3,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/projects', (request, response) => {
-    return response.send('Hello Word');
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello Word' });
 });
 
 // Porta que fica sempre 'ouvindo'
