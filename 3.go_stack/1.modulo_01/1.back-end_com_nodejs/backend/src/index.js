85a07141022aa4552d980173710df7d8548dff6e
Deivid Souza <ddeividd.eu@hotmail.com>
2020-04-14 14:43:51 -0300

    Instalar e configurar Nodemon

--- 3.go_stack/1.modulo_01/1.back-end_com_nodejs/backend/src/index.js
+++ 3.go_stack/1.modulo_01/1.back-end_com_nodejs/backend/src/index.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello Word' });
+    return response.json({ message: 'Hello World' });
 });
 
 // Porta que fica sempre 'ouvindo'
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('🚀️ Back-end started!'); 
+});
