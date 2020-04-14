32629c498e54d6262f540806ad8196b4a7c98f6e
Deivid Souza <ddeividd.eu@hotmail.com>
2020-04-14 15:16:03 -0300

    Configurar métodos HTTP

--- 3.go_stack/1.modulo_01/1.back-end_com_nodejs/backend/src/index.js
+++ 3.go_stack/1.modulo_01/1.back-end_com_nodejs/backend/src/index.js
@@ -3,8 +3,38 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
+// Listar informações
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+    ]);
+});
+
+// Gravar informações
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+// Alterar informações
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+// Deletar informações
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3',
+    ]);
 });
 
 // Porta que fica sempre 'ouvindo'
