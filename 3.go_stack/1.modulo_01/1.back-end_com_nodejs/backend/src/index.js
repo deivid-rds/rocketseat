5869c79f37e15406cbb176242e61214bb640efaa
Deivid Souza <ddeividd.eu@hotmail.com>
2020-04-15 13:59:29 -0300

    Adicionar filtro na rota de listagem

--- 3.go_stack/1.modulo_01/1.back-end_com_nodejs/backend/src/index.js
+++ 3.go_stack/1.modulo_01/1.back-end_com_nodejs/backend/src/index.js
@@ -11,12 +11,13 @@ const projects = [];
 // Listar informações
 app.get('/projects', (request, response) => {
 
-    // const { title, owner } = request.query;
+    const { title } = request.query;
 
-    // console.log(title);
-    // console.log(owner);
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
 
-    return response.json([projects]);
+    return response.json([results]);
 });
 
 // Gravar informações
