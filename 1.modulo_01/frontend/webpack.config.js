932eab920335065bde9873ef495039ffd56f2f33
Deivid Souza <ddeividd.eu@hotmail.com>
2020-04-26 15:35:12 -0300

    Configurar Webpack

---
+++ 1.modulo_01/frontend/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    
+    entry: path.resolve(__dirname, 'src', 'index.js'), /* O mesmo que 
+    './src/index.js', mas o caminho no Windows por exemplo já seria diferente 
+    já que a barra é invertida, então com path se evita esse problema.*/
+
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,   //Expressão regular para pegar todo arquivo terminado com .js 
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+};
