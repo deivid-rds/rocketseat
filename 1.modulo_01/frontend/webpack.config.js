4e3b7b4051a70fb4522218a492c99c0186684596
Deivid Souza <ddeividd.eu@hotmail.com>
2020-04-27 10:40:50 -0300

    Importando CSS

--- 1.modulo_01/frontend/webpack.config.js
+++ 1.modulo_01/frontend/webpack.config.js
@@ -17,13 +17,24 @@ module.exports = {
 
     module: {
         rules: [
+            
             {
                 test: /\.js$/,   //Expressão regular para pegar todo arquivo terminado com .js 
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                 }
+            },
+
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                ]
             }
+
         ]
     },
 };
