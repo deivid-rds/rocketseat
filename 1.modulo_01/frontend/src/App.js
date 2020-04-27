8cee93fbdc077332fd03657afb61a445b5b667d8
Deivid Souza <ddeividd.eu@hotmail.com>
2020-04-27 11:00:40 -0300

    Importando imagem

--- 1.modulo_01/frontend/src/App.js
+++ 1.modulo_01/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import './App.css';
+import backgroundImage from './assets/background.jpeg';
 
 import Header from './components/Header'
 
@@ -18,6 +19,8 @@ function App() {
     return (
         <>
             <Header title="Projects" />
+
+            <img width={300} src={backgroundImage} />
 
             <ul>
                 {projects.map(project => <li key={project}>{project}</li>)}
