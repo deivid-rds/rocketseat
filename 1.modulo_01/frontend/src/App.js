287045bb8933350aeaed2b4718df5c6018dd13c1
Deivid Souza <ddeividd.eu@hotmail.com>
2020-05-14 09:21:21 -0300

    Instalar e configurar Axios

--- 1.modulo_01/frontend/src/App.js
+++ 1.modulo_01/frontend/src/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpeg';
 
 import Header from './components/Header'
 
 function App() {
 
     const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            console.log(response);
+        });
+    }, []);
 
     function handleAddProject() {
         setProjects([...projects, `Novo projeto ${Date.now()}`]);   
@@ -19,8 +25,6 @@ function App() {
     return (
         <>
             <Header title="Projects" />
-
-            <img width={300} src={backgroundImage} />
 
             <ul>
                 {projects.map(project => <li key={project}>{project}</li>)}
